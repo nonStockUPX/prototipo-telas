cd4b98ed06ae142cb85ab19d6911e0188a823568
Augusto Martinez <augusto.mmartinez@gmail.com>
2024-04-17 20:48:59 -0300

    Criação da fábrica de conexões para ligar o sistema com o mysql

---
+++ Site/classes/ConnectionFactory.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+class ConnectionFactory{
+    constructor(config){
+        this.connection = mysql.createConnection(config);
+    }
+    connect(){
+        this.connection.connect((err) => {
+            if(err){
+                console.error('Erro ao Conectar ao Servidor', err);
+                throw err;
+            }
+            console.log('Conexão bem sucedida com o servidor')
+        });
+    }
+    query(sql, args){
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) =>{
+                if (err) return reject(err);
+                resolve(err);
+            })
+        });
+    }
+    close(){
+        this.connection.end((err) => {
+            if(err){
+                console.error('Erro ao fechar a conexão com o servidor');
+                throw err;
+            }
+            console.log('Conexão fechada com sucesso');
+        });
+    }
+}
+
+module.exports = ConnectionFactory;
