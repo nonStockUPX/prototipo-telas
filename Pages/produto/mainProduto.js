fc679bbbc3b9d1a90bec92a98e3d6f656f4b25a7
nonStockUPX <164097112+nonStockUPX@users.noreply.github.com>
2024-06-04 13:54:34 -0300

    Update mainProduto.js

--- Pages/produto/mainProduto.js
+++ Pages/produto/mainProduto.js
@@ -1,11 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const produtoGrande = document.getElementById('produtoGrande');
-    const thumbnails = document.querySelectorAll('.thumbnail');
+document.addEventListener("DOMContentLoaded", function() {
+    function getQueryParam(param) {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(param);
+    }
 
-    thumbnails.forEach(thumbnail => {
-        thumbnail.addEventListener('click', function() {
-            const src = thumbnail.getAttribute('src');
-            produtoGrande.setAttribute('src', src);
-        });
-    });
+    const produtoId = getQueryParam('id'); 
+    console.log(produtoId)
+    fetch(`https://664139c7a7500fcf1a9fdfda.mockapi.io/produto/produtos/${produtoId}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const conteudoDiv = document.querySelector('main');
+                const produtoHTML = `
+                    <div class="conteudo">
+                        <img src="${data.url_img}" alt=""> 
+                        <div class="description">
+                            <h2>${data.nome}</h2>
+                            <p>${data.descricao}</p>
+                            <ul>
+                                <li><strong>Quantidade:</strong> ${data.qtd_produto} un.</li>
+                                <li><strong>tamanho:</strong> ${data.tamanho}</li>
+                                <li><strong>material:</strong> ${data.material} </li>
+                                <li><strong>Empresa:</strong> ${data.nome_empresa}</li>
+                                <li><strong>Local de retirada:</strong> ${data.endereco} </li>
+                            </ul>
+                            <a href="" target="_blank" rel="noopener noreferrer" class="botaoProduto">Mandar Mensagem</a>
+                        </div>
+                    </div>
+                `;
+                conteudoDiv.innerHTML += produtoHTML;
+        })
+        .catch(error => console.error('Erro ao buscar produtos:', error));
 });
